2ee64cb24e84c2b50e5abbb4eab5e3e40d73984e
trytds <1307805140@qq.com>
2020-09-30 21:16:15 +0800

    添加项目文件。

---
+++ rc4.h
@@ -0,0 +1,28 @@
+#ifndef RC4_H
+#define RC4_H
+
+#define KEY_LENGTH 6
+#define TEXT_LENGTH 10
+
+//定义一个流密码结构体RC4_KEY_S
+typedef struct RC4_KEY_S {
+	unsigned char S[256];
+}RC4_KEY;
+
+//密钥分配函数
+void RC4_key(RC4_KEY* rc4_key, int* keylength)
+
+//加密
+//void RC4(RC4_KEY* rc4_key, unsigned char* plaintext, int* plaintext_length, int* ciphertext_length);
+
+void RC4(RC4_KEY* rc4_key, unsigned char* plaintext, int* plaintext_length, unsigned char* ciphertext)
+
+
+#endif // !RC4_H
+
+
+
+
+
+
+
